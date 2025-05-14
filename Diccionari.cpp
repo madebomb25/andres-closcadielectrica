4b57cae389e288be53387408cd8f753c1b56d533
marcsayago06 <125490947+marcsayago06@users.noreply.github.com>
2025-05-14 18:06:54 +0200

    Update Diccionari.cpp

--- Diccionari.cpp
+++ Diccionari.cpp
@@ -2,15 +2,59 @@
 #include <stdexcept>
 using namespace std;
 
-// IMPLEMENTACIÓ DE LA CLASSE Diccionari
-// (implementació de tots els mètodes especificats en el fitxer Diccionari.hpp)
+//*********************************************************
+// Constructores
+//*********************************************************
 
-// ...
+/* Pre: Cierto */
+/* Post: Crea un objeto de la clase Diccionari con un BST 
+vacío. */
+Diccionari::Diccionari() {
+    BST<ParFreq>(); 
+}
+//*********************************************************
+// Destructor
+//*********************************************************
 
-// ...
+/* Borra automáticamente el objeto al salir de un ámbito de 
+visibilidad local. */   
+Diccionari::~Diccionari() {}; 
+
 
 //*********************************************************
-void Diccionari::llegeixDeFitxer(const string &path)
+// Consultores
+//*********************************************************
+
+/* Pre: Cierto  */
+/* Post: El resultado indica si el diccionario contiene la
+palabra recibida por parámetro */	   
+bool Diccionari::conte(const string &paraula) const { 
+    /* IMPLEMENTACIÓN TEMPRANA: no tiene en cuenta
+    si coinciden en frecuencia en hacer la búsqueda. */
+    ParFreq pf(paraula, 0);
+    return dictionary.find(pf).first; 
+}
+
+/* Pre: La palabra recibida por parámetro está en el
+diccionario */
+/* Post: El resultado es la frecuencia que aparece en el
+diccionario de la palabra recibida por parámetro */	 
+int Diccionari::getFrequencia(const string &paraula) const {
+    /* IMPLEMENTACIÓN TEMPRANA: no tiene en cuenta si 
+    coinciden en frecuencia en hacer la búsqueda. */
+    ParFreq pf(paraula, 0); 
+    return dictionary.find(pf).second.getFrequencia(); 
+}
+
+//*********************************************************
+//Lectura y escritura
+//*********************************************************
+
+/* Pre: Cierto */
+/* Post: Si el path recibido per parámetro está asociado a un
+archivo, lee el archivo de pares (palabra, frecuencia), los ordena
+alfabéticamente y los distribuye en un BST balanceado. */
+void Diccionari::construeixDiccionari(const string &path)
 {
     ifstream fitxer(path);
     if (not fitxer.is_open()) {
@@ -19,12 +63,23 @@ void Diccionari::llegeixDeFitxer(const string &path)
 
     string paraula;
 	int frequencia;
-    while (fitxer >> paraula >> frequencia) {  // llegeix un parell (paraula, freq) 
-											   // per línia
-		ParFreq pf(paraula, frequencia);
-        insereix(pf);       // suposa que el diccionari té definida l'operació insert
+    while (fitxer >> paraula >> frequencia) {  
+        ParFreq pf(paraula, frequencia);
+        dictionary_vector.push_back(pf);        
     }
+
+    sort(dictionary_vector.begin(), dictionary_vector.end()); 
+    construeixArbre(dictionary, dictionary_vector); 
 }
 
-	  
 
+//*********************************************************
+//Métodos auxiliares
+//*********************************************************
+
+/* Pre: ? */
+/* Post: t contiene todos los elementos de v en una estructura BST 
+balanceada */
+void Diccionari::construeixArbre(BST<ParFreq> &t, vector<ParFreq> &v) {
+
+}
