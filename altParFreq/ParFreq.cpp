00e945dce9c99ee1027ca0c80416ce1f38857611
marcsayago06 <125490947+marcsayago06@users.noreply.github.com>
2025-05-21 21:46:31 +0200

    Add files via upload

---
+++ altParFreq/ParFreq.cpp
@@ -0,0 +1,73 @@
+#include "ParFreq.hpp"
+using namespace std;
+
+//*********************************************************
+// Constructores
+//*********************************************************
+
+/* 
+Pre: cierto. 
+Post: crea un pair con el primer atributo como una string vacía
+y el segundo atributo inicializado a 0.
+*/
+ParFreq::ParFreq() {
+	word = "";
+	frequency = 0; 
+}
+
+/* 
+Pre: cierto. 
+Post: crea un objeto de tipo pair con los atributos pasados por 
+parámetro. 
+*/
+ParFreq::ParFreq(string word, int frequency) {
+    this->word = word; 
+    this->frequency = frequency; 
+}
+
+//*********************************************************
+// Destructores
+//*********************************************************
+	   
+/* 
+Borra automáticamente el objeto al salir de un ámbito de 
+visibilidad local.   
+*/
+ParFreq::~ParFreq() {};
+
+//*********************************************************
+// Modificadores
+//*********************************************************
+	   
+//...
+
+
+//*********************************************************
+// Consultors
+//*********************************************************
+
+/* Pre: Cierto */
+/* Post: El resultado es la palabra del parámetro implícito */
+string ParFreq::getParaula() const {
+    return word; 
+}
+
+/* Pre: Cierto */
+/* Post: El resultado es la frecuencia del parámetro implícito */
+int ParFreq::getFrequencia() const {
+    return frequency;
+}
+
+/* Pre: Cierto  */
+/* Post: El resultado indica si la palabra del parámetro implícito
+es igual a la palabra del par recibido por parámetro */ 	   
+bool ParFreq::operator==(const ParFreq &pf) const {
+    return word == pf.word;
+}
+
+/* Pre: Cierto. */
+/* Post: El resultado indica si la palabra del parámetro implícito 
+es menor a la palabra del par recibido por parámetro */
+bool ParFreq::operator<(const ParFreq &pf) const {
+    return word < pf.word; 
+}
