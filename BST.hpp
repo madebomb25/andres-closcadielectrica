186efeb8fcfe37bff5e0b8e237889aa0636d8c15
marcsayago06 <125490947+marcsayago06@users.noreply.github.com>
2025-05-25 10:20:12 +0200

    Update BST.hpp

--- BST.hpp
+++ BST.hpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 /*  
- * Representació d'un arbre binari cerca o
+ * Representación de un árbol binario de búsqueda o
  * BST (binary search tree).
- * Un arbre binari de cerca és un arbre binari
- * en què el subarbre esquerre de qualsevol node
- * (si no està buit) conté valors menors que el
- * que conté aquest node, i el subarbre dret
- * (si no està buit) conté valors més grans.
+ * Un árbol binario de búsqueda es un árbol binario
+ * en el que el subárbol izquierdo de cualquier nodo
+ * (si no está vacío) contiene valores menores que el
+ * que contiene este nodo, y el subárbol derecho
+ * (si no está vacío) contiene valores mas grandes.
  */ 
 
 template <typename T>
@@ -32,37 +32,38 @@ class BST  {
 	
 	
 		//*********************************************************
-		// Mètodes privats auxiliars
+		// Mètodos privados auxiliares
 		//*********************************************************
 		
-		/* Pre: Cert */
-		/* Post: No fa res si 'node' és NULL, sinó 
-		   allibera espai de la jerarquia de nodes
-		   apuntada per 'node' */
+		/* Pre: Cierto */
+		/* Post: No hace nada si 'node' es NULL, sinó 
+		   libera espacio de la jerarquía de nodos
+		   apuntada por 'node' */
 		void clear(Item *node);
 
-		/* Pre: Cert */
-		/* Post: Afegeix un node nou amb el valor
-		   rebut per paràmetre a la jerarquia de
-		   nodes apuntada per 'node', mantenint
-		   les propietats de BST */
+		/* Pre: Cierto */
+		/* Post: Añade un nodo nuevo con el valor
+		   recibido por parámetro a la jerarquía de
+		   nodos apuntada per 'node', manteniendo
+		   las propiedades de BST */
 		Item* insert(Item *node, const T& d);
 		
-		/* Pre: Cert */
-		/* Post: El resultat és el node de la jerarquia
-		   de nodes apuntada per 'node', amb el mateix
-		   valor que el valor rebut per paràmetre */
+		/* Pre: Cierto */
+		/* Post: El resultado es el nodo de la jerarquía de nodos apuntada
+		por 'node', con el mismo valor recibido por parámetro. Si no existe
+		el nodo, entonces devolverá 'nullptr' como referencia.
+		*/
 		Item* find(Item *node, const T& d) const;
 	
 	
 	public:
 
 		//*********************************************************
-		// Constructors
+		// Constructores
 		//*********************************************************
 	
-		/* Pre: Cert */
-		/* Post: El resultat és un bst sense cap element */	
+		/* Pre: Cierto */
+		/* Post: El resultado es un BST sin ningún elemento */	
 		BST();
 
 
@@ -70,31 +71,31 @@ class BST  {
 		// Destructor
 		//*********************************************************
 	
-		/* Esborra automàticament els objectes locals
-		   en sortir d'un àmbit de visibilitat */
+		/* Borra automáticamente los objectos locales
+		   al salir de un ámbito de visibilidad */
 		~BST();
 
 		//*********************************************************
-		// Modificadors
+		// Modificadores
 		//*********************************************************
 
-		/* Pre: Cert */
-		/* Post: Afegeix un element, amb el valor rebut
-		   per paràmetre, al bst paràmetre implícit */
+		/* Pre: Cierto */
+		/* Post: Añade un elemento, con el valor recibido
+		   per parámetro, al BST parámetro implícito */
 		void insert(const T& d);
 	
 
 		//*********************************************************
-		// Consultors
+		// Consultores
 		//*********************************************************
 	
-		/* Pre: Cert */
-		/* Post: El primer component del resultat indica
-		   si el bst paràmetre implícit conté un element
-		   amb el valor rebut per paràmetre; en cas afirmatiu,
-		   el segon component conté l'element del paràmetre
-		   implícit amb aquest valor, altrament conté un
-		   element buit */
+		/* Pre: Cierto */
+		/* Post: El primer componente del resultado indica
+		   si el BST parámetro implícito contiene un elemento
+		   con el valor recibido por parámetro; en caso afirmativo,
+		   el segundo componente contiene el elemento del parámetro
+		   implícito con este valor, si no, contiene un
+		   elemento vacío */
 		pair<bool, T> find(const T& d) const;
 	
 };
